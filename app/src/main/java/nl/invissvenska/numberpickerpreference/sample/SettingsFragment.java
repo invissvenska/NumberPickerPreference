300af7866a488470d02aa8014b6d9049b4212235
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-11-24 20:02:13 +0100

    fix #9

--- app/src/main/java/nl/invissvenska/numberpickerpreference/sample/SettingsFragment.java
+++ app/src/main/java/nl/invissvenska/numberpickerpreference/sample/SettingsFragment.java
@@ -19,11 +19,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
     }
